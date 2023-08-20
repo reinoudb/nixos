f062b62c0ae830dd0ea7229cc3fe19bd5ef605bc
s141959@ap.be <nwV7'Lg^s;{^XPF~Tg*x]t?%Dg'{B8GR7{>
2023-08-20 14:22:16 +0200

    changed paths in battery alrert

--- scripts/battery/alert-battery.sh
+++ scripts/battery/alert-battery.sh
@@ -4,11 +4,9 @@ bat_status=$(cat "${bat_files}/status")
 capacity=$(cat "${bat_files}/capacity")
 
 
-
-
 if [[ ${bat_status} == "Discharging" && ${capacity} -lt	28 && ${capacity} -gt 15 ]]; then
     echo "Battery alert - ${capacity}%"
-    notify-send  "Low Battery Level" "Only ${capacity} left" --icon=/config/battery/low-battery.png --urgency normal
+    notify-send  "Low Battery Level" "Only ${capacity} left" --icon=./low-battery.png --urgency normal
 fi
 
 
@@ -16,9 +14,9 @@ fi
 
 if [[ ${bat_status} == "Discharging" && ${capacity} -lt 15 ]]; then
 	    echo "Battery alert - ${capacity}%"
-	    notify-send  "Low Battery Level" "Only ${capacity}" --icon=/config/battery/low-battery.png --urgency critical
+	    notify-send  "Low Battery Level" "Only ${capacity}" --icon=./low-battery.png --urgency critical
 		sleep 60
-    /config/battery/alert-battery.sh
+    ./alert-battery.sh
 fi
 
 
@@ -26,10 +24,10 @@ fi
 
 if [[ ${capacity} -gt 98 ]]; then
 	    echo "Battery alert - ${capacity}%"
-	    notify-send  "Battery full" --icon=/config/battery/full-battery.png --urgency low
+	    notify-send  "Battery full" --icon=./full-battery.png --urgency low
 fi
 
 echo $capacity
 notify-send "test"
 sleep 20
-/config/battery/alert-battery.sh
+./alert-battery.sh
