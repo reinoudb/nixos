c2096c611ccc25d981aac60da5ba08333a84fe88
s141959@ap.be <nwV7'Lg^s;{^XPF~Tg*x]t?%Dg'{B8GR7{>
2023-09-16 21:35:03 +0200

    added firejail for librewolf & signal

--- system/configuration.nix
+++ system/configuration.nix
@@ -255,6 +255,28 @@ virtualisation = {
 };
 
 programs = {
+  firejail = {
+  enable = true;
+  wrappedBinaries = {
+    librewolf = {
+      executable = "${pkgs.librewolf}/bin/librewolf";
+      profile = "${pkgs.firejail}/etc/firejail/librewolf.profile";
+      extraArgs = [
+        # Required for U2F USB stick
+        "--ignore=private-dev"
+        # Enforce dark mode
+        "--env=GTK_THEME=Adwaita:dark"
+        # Enable system notifications
+        "--dbus-user.talk=org.freedesktop.Notifications"
+      ];
+    };
+    signal-desktop = {
+      executable = "${pkgs.signal-desktop}/bin/signal-desktop --enable-features=UseOzonePlatform --ozone-platform=wayland";
+      profile = "${pkgs.firejail}/etc/firejail/signal-desktop.profile";
+      extraArgs = [ "--env=LC_ALL=C" "--env=GTK_THEME=Adwaita:dark" ];
+    };
+  };
+};
   i3lock.enable = true;
   ssh.knownHosts = {
       myhost = {
