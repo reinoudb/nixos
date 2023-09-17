528f496b921bee55f03a691047fec3efc3e2bbf4
s141959@ap.be <nwV7'Lg^s;{^XPF~Tg*x]t?%Dg'{B8GR7{>
2023-09-17 17:42:58 +0200

    enabled firewall explicitly

--- system/configuration.nix
+++ system/configuration.nix
@@ -1,5 +1,4 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
+# Edit this configuration file to define what should be installed on # your system.  Help is available in the configuration.nix(5) man page
 ## and in the NixOS manual (accessible by running ‘nixos-help’).
 
 { config, pkgs, inputs, ... }:
@@ -246,7 +245,7 @@ lutris
   # networking.firewall.allowedTCPPorts = [ ... ];
   # networking.firewall.allowedUDPPorts = [ ... ];
   # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
+  networking.firewall.enable = true;
 
 xdg.portal.enable = true;
 xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-kde ];
