89afe1ae74dd07dde2192e36f6b728fe4082e647
s141959@ap.be <nwV7'Lg^s;{^XPF~Tg*x]t?%Dg'{B8GR7{>
2023-09-18 21:24:00 +0200

    closed some ports

--- system/configuration.nix
+++ system/configuration.nix
@@ -243,7 +243,7 @@ lutris
 
 
   # Open ports in the firewall.
-  networking.firewall.allowedTCPPorts = [ 4713 6000 ];
+  networking.firewall.allowedTCPPorts = [];
   # networking.firewall.allowedUDPPorts = [ ... ];
   # Or disable the firewall altogether.
   networking.firewall.enable = true;
