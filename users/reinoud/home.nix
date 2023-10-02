7bcf2291b6314c9b8867f7c43278c8c46d9426ae
s141959@ap.be <nwV7'Lg^s;{^XPF~Tg*x]t?%Dg'{B8GR7{>
2023-10-02 11:19:01 +0200

    ricing lf

--- users/reinoud/home.nix
+++ users/reinoud/home.nix
@@ -104,9 +104,14 @@
         icons = true;
        ignorecase = true; 
       };
+      keybindings = {
+        "<enter>" = "open";
+        V = ''$${pkgs.bat}/bin/bat "$f"'';
+      };
       commands = {
         dragon-out = ''${pkgs.xdragon}/bin/xdragon -a -x "$fx"'';
         editor-open = ''$$EDITOR $f'';
+        e = ''$$EDITOR $f'';
         mkdir = ''
          ''${{
             printf "Directory Name: " 
@@ -114,11 +119,64 @@
             mkdir $DIR
           }}
         '';
+        extract = ''
+          ''${{
+            set -f
+            case $f in
+            *.tar.bz|*.tar.bz2|*.tbz|*.tbz2) tar xjvf $f;;
+            *.tar.gz|*.tgz) tar xzvf $f;;
+            *.tar.xz|*.txz) tar xJvf $f;;
+            *.zip) unzip $f;;
+            *.rar) unrar x $f;;
+            *.7z) 7z x $f;;
+            esac
+          }}
+        '';
+        tar = ''
+          ''${{
+          set -f
+          mkdir $1
+          cp -r $fx $1
+          tar czf $1.tar.gz $1
+          rm -rf $1
+        }}'';
+        zip = ''
+          ''${{
+          set -f
+          mkdir $1
+          cp -r $fx $1
+          zip -r $1.zip $1
+          rm -rf $1
+        }}'';
       }; 
-      keybindings = {
-        ee = "editor-open";
-      }; 
-    };
+
+      extraConfig = 
+          let 
+            previewer = 
+              pkgs.writeShellScriptBin "pv.sh" ''
+              file=$1
+              w=$2
+              h=$3
+              x=$4
+              y=$5
+              
+              if [[ "$( ${pkgs.file}/bin/file -Lb --mime-type "$file")" =~ ^image ]]; then
+                  ${pkgs.kitty}/bin/kitty +kitten icat --silent --stdin no --transfer-mode file --place "''${w}x''${h}@''${x}x''${y}" "$file" < /dev/null > /dev/tty
+                  exit 1
+              fi
+              
+              ${pkgs.pistol}/bin/pistol "$file"
+            '';
+            cleaner = pkgs.writeShellScriptBin "clean.sh" ''
+              ${pkgs.kitty}/bin/kitty +kitten icat --clear --stdin no --silent --transfer-mode file < /dev/null > /dev/tty
+            '';
+          in
+          ''
+            set cleaner ${cleaner}/bin/clean.sh
+            set previewer ${previewer}/bin/pv.sh
+          '';
+      };
+
     home-manager.enable = true;
      fish = {
       enable = true;
