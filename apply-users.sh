6c036a2559598bb4e618a8dffe253eac1cf32cf5
s141959@ap.be <nwV7'Lg^s;{^XPF~Tg*x]t?%Dg'{B8GR7{>
2023-10-17 16:37:21 +0200

    setting up flake in hm

--- apply-users.sh
+++ apply-users.sh
@@ -1,5 +1,6 @@
 #!/bin/sh 
 pushd ~/.dotfiles/
-home-manager switch -f ./users/reinoud/home.nix
+# home-manager switch -f ./users/reinoud/home.nix
+home-manager switch --flake ./users/reinoud/
 popd
 
