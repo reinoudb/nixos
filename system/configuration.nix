82dc634a1d7d8de5e118e295250b71daaee3d5e5
GrannyCadet <s141959@ap.be>
2023-11-13 20:52:06 +0100

    borgbackup only to 192.168.0.125

--- system/configuration.nix
+++ system/configuration.nix
@@ -277,7 +277,7 @@ services = {
       passCommand = "cat /home/reinoud/.dotfiles/secrets/borg/password";
       };
     environment.BORG_RSH = "ssh -i /home/reinoud/.ssh/id_rsa";
-    repo = "ssh://reinoud@perfectekindje.airdns.org:11318/mnt/2tb/backup/nixos";
+    repo = "ssh://reinoud@192.168.0.125:57130/mnt/2tb/backup/nixos";
     compression = "auto,zstd";
     startAt = "hourly";
     prune.keep = {
