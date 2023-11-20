28036d9b2dca839c8c62c36d432244e341233c52
GrannyCadet <s141959@ap.be>
2023-11-20 21:47:07 +0100

    fixed script

--- programs/dmenu/scripts/games.sh
+++ programs/dmenu/scripts/games.sh
@@ -1,15 +1,16 @@
 #!/bin/sh
 
-gamedir="$HOME/Games"
-games=$(ls $gamedir)
+games_dir="$HOME/Games"
+games=$(ls $games_dir)
 
 keuze=$(echo -e "$games" | dmenu -i -p "Choose your game")
 
 if [ -n "$keuze" ]; then
   cd "$games_dir/$keuze"
+  echo "$games_dir/$keuze"
   start_script=$(ls start*)
 
-  if [ -n "$start_script" ]; then
+  if [ -e "$start_script" ]; then
     bash "$start_script"
   else
     echo -e "ok" | dmenu -i -p "No start script found in the chosen game directory."
