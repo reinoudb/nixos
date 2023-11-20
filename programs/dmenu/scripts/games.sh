769251f65cc0241c34c593774e2beadceb287cae
GrannyCadet <s141959@ap.be>
2023-11-20 21:32:08 +0100

    updating script

--- programs/dmenu/scripts/games.sh
+++ programs/dmenu/scripts/games.sh
@@ -1,7 +1,19 @@
 #!/bin/sh
 
-games=$(ls ~/Games)
+gamedir="$HOME/Games"
+games=$(ls $gamedir)
 
 keuze=$(echo -e "$games" | dmenu -i -p "Choose your game")
-cd ~/Games/$keuze
-bash start*
+
+if [ -n "$keuze" ]; then
+  cd "$games_dir/$keuze"
+  start_script=$(ls start*)
+
+  if [ -n "$start_script" ]; then
+    bash "$start_script"
+  else
+    echo -e "ok" | dmenu -i -p "No start script found in the chosen game directory."
+  fi
+else
+  echo "no game selected"
+fi
