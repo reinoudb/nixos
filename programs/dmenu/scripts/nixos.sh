53d1c60c29ecaf28cc41304aac457627d463530f
GrannyCadet <s141959@ap.be>
2023-12-11 08:29:15 +0100

    starting with nixos script for dmenu

--- programs/dmenu/scripts/nixos.sh
+++ programs/dmenu/scripts/nixos.sh
@@ -1,4 +1,17 @@
 #!/bin/sh
 # dmenu script to rebuild update rebuild custom path home manager etc for nixos
 
+rebuild () {
+  alacritty -e sudo nixos-rebuild $1 $2
+}
 
+choises="switch\ntest\nupgrade\ndry-activate"
+
+keuze=$(echo -e $choises | dmenu -i -p "How to rebuild?")
+
+case $keuze in
+  switch) rebuild switch;;
+  test) rebuild test;;
+  upgrade) switch --upgrade;;
+  dry-activate) switch --dry-activate;;
+esac
