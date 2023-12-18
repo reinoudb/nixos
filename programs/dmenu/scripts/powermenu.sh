f4776259aeb651f23525b0bb59ed0ba5717cf519
GrannyCadet <s141959@ap.be>
2023-12-18 20:52:26 +0100

    removed -u with i3lock

--- programs/dmenu/scripts/powermenu.sh
+++ programs/dmenu/scripts/powermenu.sh
@@ -5,7 +5,7 @@ choises="Lock\nLogout\nSleep\nHibernate\nReboot\nShutdown"
 
 keuze=$(echo -e "$choises" | dmenu -i)
 case "$keuze" in
-  Lock) i3lock -u -i /home/reinoud/Pictures/wallpaper/nix-wallpaper-stripes-logo.png -t -e;;
+  Lock) i3lock -i /home/reinoud/Pictures/wallpaper/nix-wallpaper-stripes-logo.png -t -e;;
   Logout) pkill -KILL -u $(whoami);;
   Sleep) systemctl suspend;;
   Hibernate) systemctl hibernate;;
